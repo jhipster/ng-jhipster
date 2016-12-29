2af9491d292128406d0f04d22be64661fa06ed37
SendilKumar N <sendilkumarn@live.com>
2016-12-29 19:53:01 +0800

    Added pagination util service and test

--- src/service/pagination-util.service.ts
+++ src/service/pagination-util.service.ts
@@ -1,25 +1,53 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { Injectable } from '@angular/core';
 
+/**
+ * An utility service for pagination
+ */
 @Injectable()
 export class PaginationUtil  {
 
-    parseAscending (sort: string) {
+    constructor() {}
+
+    /**
+     * Method to find whether the sort is defined
+     */
+    parseAscending (sort: string): boolean {
         let sortArray = sort.split(',');
         sortArray = sortArray.length > 1 ? sortArray : sort.split('%2C');
         if (sortArray.length > 1) {
             return sortArray.slice(-1)[0] === 'asc';
         }
-        // default to true if no sort defined
+        // default to true if no sort is defined
         return true;
     }
 
-    // query params are strings, and need to be parsed
-    parsePage (page: string) {
+    /**
+     * Method to query params are strings, and need to be parsed
+     */
+    parsePage (page: string): number {
         return parseInt(page, 10);
     }
 
-    // sort can be in the format `id,asc` or `id`
-    parsePredicate (sort: string) {
+    /**
+     * Method to sort can be in the format `id,asc` or `id`
+     */
+    parsePredicate (sort: string): string {
         let sortArray = sort.split(',');
         if (sortArray.length > 1) {
             sortArray.pop();
