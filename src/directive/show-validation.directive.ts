e05a007fd31481ad759d8945df443bb7eb81b973
SendilKumar N <sendilkumarn@live.com>
2017-01-03 01:12:39 +0800

    replaced with arrow function

--- src/directive/show-validation.directive.ts
+++ src/directive/show-validation.directive.ts
@@ -19,22 +19,22 @@ import { Directive, ElementRef, Renderer } from '@angular/core';
 export class ShowValidationDirective {
     constructor(el: ElementRef, renderer: Renderer) {
        let formElements = el.nativeElement.querySelectorAll('.form-group');
-       Array.prototype.forEach.call(formElements, (formGroup: any) => {
-           // TODO this needs to be properly tested
+       formElements.forEach((formGroup) =>  {
            let inputs = formGroup.querySelectorAll('input[(ngModel)],textarea[(ngModel)],select[(ngModel)]');
 
-           if (inputs !== null) {
-               Array.prototype.forEach.call(inputs, (input: any) => {
-                   // TODO this logic needs to be checked and fixed accordingly
-                   let isInvalid = input.classList.contains('ng-invalid') && input.classList.contains('ng-dirty');
-                   formGroup.classList.toggle('has-error', isInvalid);
-                   /*scope.$watch(function() {
+           if (inputs) {
+               inputs.forEach((input) => {
+                    let isInvalid = input.classList.contains('ng-invalid') && input.classList.contains('ng-dirty');
+                    formGroup.classList.toggle('has-error', isInvalid);
+
+                    /*scope.$watch(function() {
                        return $input.hasClass('ng-invalid') && $input.hasClass('ng-dirty');
                    }, function(isInvalid) {
                        $formGroup.toggleClass('has-error', isInvalid);
                    });*/
                });
            }
+
        });
     }
 }
