9c94ec048af75041b4f86ee02a9aa8d17c0e0516
VictorADS <victor.ads75@gmail.com>
2017-01-06 16:47:56 +0100

    Linting

--- src/pipe/pure-filter.pipe.ts
+++ src/pipe/pure-filter.pipe.ts
@@ -1,5 +1,9 @@
-import { Pipe } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 import { FilterPipe } from './filter.pipe';
 
 @Pipe({ name: 'pureFilter' })
-export class PureFilterPipe extends FilterPipe {}
+export class PureFilterPipe extends FilterPipe implements PipeTransform {
+    transform(input: Array<any>, filter: string, field: string): any {
+        return super.transform(input, filter, field);
+    }
+}
