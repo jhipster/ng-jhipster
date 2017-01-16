a2315cb35fc78a6650f8db891879875fa006766d
Deepu K Sasidharan <d4udts@gmail.com>
2017-01-16 20:44:00 +0100

    remove showValidation directive

--- src/directive/index.ts
+++ src/directive/index.ts
@@ -1,5 +1,4 @@
 export * from './maxbytes.directive';
 export * from './minbytes.directive';
-export * from './show-validation.directive';
 export * from './sort.directive';
 export * from './sort-by.directive';
