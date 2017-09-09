2f8a2a9223ab7b9c2a7986d3c35dfbae1e123f6b
Stephen Doxsee <sdoxsee@users.noreply.github.com>
2017-09-09 13:49:26 -0400

    Update data-util.service.ts
    Fix wrong variable names for type and data

--- src/service/data-util.service.ts
+++ src/service/data-util.service.ts
@@ -48,7 +48,7 @@ export class JhiDataUtils {
      * Method to open file
      */
     openFile(type: string, data: string) {
-        const fileURL = 'data:' + contentType + ';base64,' + field;
+        const fileURL = 'data:' + type + ';base64,' + data;
         const win = window.open();
         win.document.write(
             '<iframe src="' + fileURL + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
