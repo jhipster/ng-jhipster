566079664b990bad7d977cb211febeedb17a5f53
Guillaume Dufour <guillaume.duff@gmail.com>
2018-04-24 22:40:14 +0200

    add new files in index

--- src/directive/index.ts
+++ src/directive/index.ts
@@ -18,5 +18,7 @@
  */
 export * from './maxbytes.directive';
 export * from './minbytes.directive';
+export * from './max.directive';
+export * from './min.directive';
 export * from './sort.directive';
 export * from './sort-by.directive';
