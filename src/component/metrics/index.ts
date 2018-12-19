f5289af74dc78fb8b1a1a43e665f887aa5ab3409
Clément Dessoude <clement.dessoude@gmail.com>
2018-12-19 15:31:17 +0100

    rename and refactor serviceMetrics to endpointsRequestsMetrics
    list and give count and mean access time on each each endpoint by CRUD
    operation

--- src/component/metrics/index.ts
+++ src/component/metrics/index.ts
@@ -20,7 +20,7 @@ export * from './jhi-jvm-memory.component';
 export * from './jhi-jvm-threads.component';
 export * from './jhi-metrics-modal-threads.component';
 export * from './jhi-metrics-request.component';
-export * from './jhi-metrics-services.component';
+export * from './jhi-metrics-endpoints-requests';
 export * from './jhi-metrics-cache.component';
 export * from './jhi-metrics-datasource.component';
 export * from './jhi-metrics-system.component';
